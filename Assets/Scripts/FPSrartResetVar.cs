27194a6543ec30c529a527f71b3cdfa83e5a4fc3
Kurata-Note <mkh78864@gmail.com>
2023-02-06 13:57:46 +0900

    バックアップ

---
+++ Assets/Scripts/FPSrartResetVar.cs
@@ -0,0 +1,11 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class FPSrartResetVar : MonoBehaviour
+{
+    public void ResetStVars(){
+        StaticVar.type = "N";
+        StaticVar.stage = 1;
+    }
+}
